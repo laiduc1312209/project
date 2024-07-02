96ef4f84ae894fe8b3247079e457ddc893c32a20
laiduc1312209 <publinedev@gmail.com>
2024-07-02 19:31:29 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,16 +1,14 @@
-import requests
+import wmi
 
-def load_and_run_code_from_url(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Kiểm tra lỗi HTTP
-        code = response.text
-        exec(code)  # Thực thi mã đã tải
-    except requests.RequestException as e:
-        print(f"Lỗi khi tải dữ liệu từ URL: {e}")
-    except Exception as e:
-        print(f"Lỗi khi thực thi mã: {e}")
+def get_hwid():
+    c = wmi.WMI()
+    hwid = []
+
+    for item in c.Win32_ComputerSystemProduct():
+        hwid.append(item.UUID)
+
+    return hwid
 
 if __name__ == "__main__":
-    url = "https://raw.githubusercontent.com/laiduc1312209/project/main/a.py"
-    load_and_run_code_from_url(url)
+    hwid = get_hwid()
+    print("Hardware ID(s):", hwid)
