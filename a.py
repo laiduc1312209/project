3c6574e90482f2f51143f00db605886eb02d2a65
laiduc1312209 <publinedev@gmail.com>
2024-07-02 19:33:28 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,14 +1,1 @@
-import wmi
-
-def get_hwid():
-    c = wmi.WMI()
-    hwid = []
-
-    for item in c.Win32_ComputerSystemProduct():
-        hwid.append(item.UUID)
-
-    return hwid
-
-if __name__ == "__main__":
-    hwid = get_hwid()
-    print("Hardware ID(s):", hwid)
+print('duccuto')
