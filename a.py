83e3fe2510412472c25ba6e3e455e209f9440c73
laiduc1312209 <publinedev@gmail.com>
2024-07-02 19:42:23 +0700

    Update a.py

--- a.py
+++ a.py
@@ -8,7 +8,38 @@ def get_hwid():
         hwid.append(item.UUID)
 
     return hwid
+import requests
+import re
+import os
+import colorama
+from colorama import Fore, Style
+os.system("mode con: cols=70 lines=25")
+os.system("cls")
+colorama.init()
 
+
+print(colorama.Fore.CYAN +" __        _ ____          ")
+print(colorama.Fore.CYAN +"|  |   ___|_|    | _ _ ___ ")
+print(colorama.Fore.CYAN +"|  |__| .'| |  |  | | |  _|")
+print(colorama.Fore.CYAN +"|_____|__,|_|____||___|___|")
+print(colorama.Fore.RED + '\n-Tool bypass key executor-')
 if __name__ == "__main__":
     hwid = get_hwid()
-    print("Hardware ID(s):", hwid)
+    print(colorama.Fore.MAGENTA + "Hardware ID(s):", hwid)
+print(colorama.Fore.YELLOW + "[+] Executor support: Fluxus, Delta, CodeX")
+key = input('\n[?] Input your key : ')
+api_url = f"https://shouko.goatbypassers.xyz/api/bypass?link={key}&api_key=goatbypassersontop"
+response = requests.get(api_url)
+if response.status_code == 200:
+    content = response.content.decode()
+    key_search = re.search('"key":"(.*?)"', content)
+    if key_search:
+        key = key_search.group(1)
+        print(f"\n\033[1;32m [ OUTPUT ] \033[1;37m: \033[1;33m{key}" + Style.RESET_ALL)
+    else:
+        print(Fore.RED + "\nEROR" + Style.RESET_ALL)
+else:
+    print(Fore.RED + f"\nRequest failed, code status: {response.status_code}" + Style.RESET_ALL)
+
+input(Fore.GREEN + "\nPress Enter key to exit..." + Style.RESET_ALL)
+os.system('exit')
